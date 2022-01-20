d043afa83c8d4e39b5386121d5b01d8963f80340
Andrii <adrobotyuk@gmail.com>
2022-01-20 14:54:00 +0600

    new

---
+++ <script>.js
@@ -0,0 +1,13 @@
+function stringToArray (string){
+    return console.log(string.split (' '));
+}
+
+stringToArray ("Robin Singh")
+
+
+function fakeBin(x){
+    return console.log(x.split('').map(i => i < 5 ? 0 : 1).join());
+}
+
+fakeBin('45385593107843568')
+
